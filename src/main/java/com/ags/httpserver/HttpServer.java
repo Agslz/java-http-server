233850183626ca2548f345ab39813413a8234a55
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-11 09:56:03 -0300

    Adding Configuration Manager and Docs

--- src/main/java/com/ags/httpserver/HttpServer.java
+++ src/main/java/com/ags/httpserver/HttpServer.java
@@ -1,4 +1,8 @@
 package com.ags.httpserver;
+
+import com.ags.httpserver.config.Configuration;
+import com.ags.httpserver.config.ConfigurationManager;
+
 /**
  *
  * Driver Class for the Http Server
@@ -10,6 +14,14 @@ public class HttpServer {
 
         System.out.println("Server Starting...");
 
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Using Port: "+ conf.getPort());
+        System.out.println("Using Webroot: "+conf.getWebroot());
+
     }
+
 
 }
--- src/main/java/com/ags/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/ags/httpserver/config/ConfigurationManager.java
@@ -1,5 +1,13 @@
 package com.ags.httpserver.config;
 
+import com.ags.httpserver.utils.Json;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
 
     private static ConfigurationManager myConfigurationManager;
@@ -15,18 +23,49 @@ public class ConfigurationManager {
     }
 
     /**
+     * Loads a configuration file from the specified file path.
      *
-     * Used to load a configuration file by path provided
+     * @param filePath The path to the configuration file to load.
+     * @throws HttpConfigurationException If an error occurs during the configuration loading process,
+     *                                     including file not found, I/O errors, or JSON parsing errors.
+     *                                     This exception wraps the underlying exceptions for better error handling.
      */
-    public void loadConfigurationFile(String filePath){
-
+    public void loadConfigurationFile(String filePath) {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfigurationException(e);
+        }
+        StringBuffer sb = new StringBuffer();
+        int i;
+        try{
+            while (( i = fileReader.read()) != -1){
+                sb.append((char) i);
+            }
+        }catch (IOException e){
+            throw new HttpConfigurationException(e);
+        }
+        JsonNode conf = null;
+        try {
+            conf = Json.parse(sb.toString());
+        } catch (JsonProcessingException e) {
+            throw new HttpConfigurationException("Error parsing the configuration file", e);
+        }
+        try {
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error parsing the configuration file, internal",e);
+        }
     }
 
     /**
-     *
      * Returns the Current loaded Configuration
      */
-    public void getCurrentConfiguration(){
-
+    public Configuration getCurrentConfiguration() {
+        if(myCurrentConfiguration == null){
+         throw new HttpConfigurationException("No current configurarion set.");
+        }
+        return myCurrentConfiguration;
     }
 }
