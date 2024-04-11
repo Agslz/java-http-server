458976960b2abce5724352df606a1649d0dfe996
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-11 12:16:05 -0300

    Refactor code and Using Multiple Threads

--- src/main/java/com/ags/httpserver/HttpServer.java
+++ src/main/java/com/ags/httpserver/HttpServer.java
@@ -2,71 +2,38 @@ package com.ags.httpserver;
 
 import com.ags.httpserver.config.Configuration;
 import com.ags.httpserver.config.ConfigurationManager;
+import com.ags.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Driver Class for the Http Server
  */
 public class HttpServer {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
     public static void main(String[] args) {
 
-        System.out.println("Server Starting...");
+        LOGGER.info("Server Starting");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("Using Port: " + conf.getPort());
-        System.out.println("Using Webroot: " + conf.getWebroot());
+        LOGGER.info("Using Webroot: {}", conf.getWebroot());
+        LOGGER.info("Using Port: {}", conf.getPort());
 
+
+        ServerListenerThread serverListenerThread = null;
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = """
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                    <title>Simple Java HTTP Server</title>
-                    </head>
-                    <body>
-                    <h1>This page was served using my simple Java HTTP Server</h1>
-                    </body>
-                    </html>
-                    """;
-
-
-            final String CRLF = "\n\r"; // 13, 10 ASCII
-
-            String response =
-                    //Status Line : HTTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "HTTP/1.1 200 OK" + CRLF +
-                            //Header
-                            "Content-Length:" + html.getBytes().length + CRLF +
-                            CRLF + html + CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            // TODO we would writing
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
-
 
 }
